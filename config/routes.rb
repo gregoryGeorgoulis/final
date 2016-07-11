502eade4380dc898cd07e3637bf08a371bb4038b
Cat Georgoulis <catmou@gregorys-MacBook-Pro.local>
2016-07-11 12:20:05 -0400

    ajax call works baby

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
  root 'states#index'
  get '/state/:id' => 'states#show', as: 'state'
+ get '/states/polster', to: 'states#polster'
 
-#  Prefix Verb URI Pattern        Controller#Action
-#    root GET  /                  states#index
-# newyork GET  /newyork(.:format) states#show
+#  Prefix Verb URI Pattern              Controller#Action
+#          root GET  /                        states#index
+#         state GET  /state/:id(.:format)     states#show
+# state_polster GET  /state/polster(.:format) states#polster
 end
